5790be3cab643ea7170ce0f3fb68f971329e0a88
Andy Delcambre <andy@github.com>
2013-04-03 12:25:11 -0700

    Add custom directory

---
+++ custom/aliases.zsh
@@ -0,0 +1,4 @@
+alias git='hub'
+alias b='bundle-hack'
+alias gh-reload='source /opt/github/env.sh'
+alias gi='git'
---
+++ custom/functions.zsh
@@ -0,0 +1,22 @@
+# Open/clone project
+p() {
+  if [[ -d ~/github/$1 ]]; then
+    cd ~/github/$1
+  else
+    if [[ -f $BOXEN_HOME/repo/modules/projects/manifests/$1.pp ]]; then
+      boxen $1 && cd ~/github/$1
+    else
+      cd ~/github
+      git clone git@github.com:github/$1.git && cd ~/github/$1
+    fi
+  fi
+}
+
+# Find files matching name
+f() {
+  if [[ $# -eq 1 ]]; then
+    find . -name "*$1*"
+  elif; then
+    find $2 -name "*$1*"
+  fi
+}
