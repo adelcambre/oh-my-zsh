e6ea52018d21d4d4ffda1a88bab6552020f479ac
Andy Delcambre <andy@github.com>
2013-09-30 12:33:49 -0700

    Tweak custom functions

--- custom/functions.zsh
+++ custom/functions.zsh
@@ -4,7 +4,7 @@ p() {
     cd ~/github/$1
   else
     if [[ -f $BOXEN_HOME/repo/modules/projects/manifests/$1.pp ]]; then
-      boxen $1 && cd ~/github/$1
+      boxen $1 ; cd ~/github/$1
     else
       cd ~/github
       git clone git@github.com:github/$1.git && cd ~/github/$1
@@ -15,7 +15,7 @@ p() {
 # Find files matching name
 f() {
   if [[ $# -eq 1 ]]; then
-    find . -name "*$1*"
+    find . -name "*$1*" -not -ipath '*.git*' -not -ipath '*/vendor/*'
   elif; then
     find $2 -name "*$1*"
   fi
