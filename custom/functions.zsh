be5126751a63fd8b4dcb0ca5847e51eb29d1bf11
Andy Delcambre <adelcambre@gmail.com>
2015-10-08 16:04:26 -0600

    Update functions

--- custom/functions.zsh
+++ custom/functions.zsh
@@ -1,21 +1,17 @@
 # Open/clone project
 p() {
-  if [[ -d ~/github/$1 ]]; then
-    cd ~/github/$1
+  if [[ -d ~/p/$1 ]]; then
+    cd ~/p/$1
   else
-    if [[ -f $BOXEN_HOME/repo/modules/projects/manifests/$1.pp ]]; then
-      boxen $1 ; cd ~/github/$1
-    else
-      cd ~/github
-      git clone git@github.com:github/$1.git && cd ~/github/$1
-    fi
+    cd ~/p
+    git clone git@github.com:github/$1.git && cd ~/p/$1
   fi
 }
 
 # Find files matching name
 f() {
   if [[ $# -eq 1 ]]; then
-    find . -name "*$1*" -not -ipath '*.git*' -not -ipath '*/vendor/*'
+    find . -name "*$1*" -not -ipath '*.git*' -not -ipath '*/vendor/gems/*'
   elif; then
     find $2 -name "*$1*"
   fi
